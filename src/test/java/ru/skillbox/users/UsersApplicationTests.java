042fd94430ce87ff2fe0bf616f42db4f82265a9a
Marat Murtuzaliev <marat.murtuzaliev@gmail.com>
2024-07-18 16:33:11 +0300

    Initial commit

---
+++ src/test/java/ru/skillbox/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
